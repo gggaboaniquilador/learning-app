1f05281fef7435ae3583625efea03ae410e4ef09
Gabriel Perez <savaalexandru180@gmail.com>
2023-02-28 12:42:35 -0400

    initial configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BooksModule } from './books/books.module';
+import { UsersModule } from './users/users.module';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    MongooseModule.forRoot(
+      'mongodb+srv://gabo:gabo@cluster0.fvqm4.mongodb.net/?retryWrites=true&w=majority',
+    ),
+    BooksModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
